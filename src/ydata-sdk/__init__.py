aa3885a84e0f1ebb031dda7a7de8d8d64897b2f9
Vasco Ramos <vasco.ramos@ydata.ai>
2023-01-23 13:40:36 +0000

    fix dev default version

--- src/ydata-sdk/__init__.py
+++ src/ydata-sdk/__init__.py
@@ -5,4 +5,4 @@ source_root = Path(".")
 try:
     __version__ = (source_root / "VERSION").read_text().rstrip("\n")
 except FileNotFoundError:
-    __version__ = "0.0.0"
+    __version__ = "0.0.dev0"
