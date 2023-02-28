1a7d057628c37ea46ca3143650801c36007e558c
Alexandre Quemy <alexandre.quemy@gmail.com>
2023-02-28 13:26:45 +0100

    chore: move the warning to common

--- src/ydata/sdk/__init__.py
+++ src/ydata/sdk/__init__.py
@@ -1,10 +1,4 @@
-from os import environ
 from pathlib import Path
-from warnings import warn
-
-from ydata.sdk.common.client.client import HELP_TEXT
-from ydata.sdk.common.config import TOKEN_VAR
-from ydata.sdk.common.warnings import NewUserWarning
 
 source_root = Path(".")
 
@@ -12,7 +6,3 @@ try:
     __version__ = (source_root / "VERSION").read_text().rstrip("\n")
 except FileNotFoundError:
     __version__ = "0.0.dev0"
-
-token = environ.get(TOKEN_VAR)
-if token is None:
-    warn(HELP_TEXT, NewUserWarning)
