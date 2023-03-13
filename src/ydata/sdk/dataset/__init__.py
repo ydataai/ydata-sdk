82a443e367408c466b155a87d5298c7749dcedca
Vasco Ramos <vasco.ramos@ydata.ai>
2023-03-13 10:23:26 +0000

    include missing exports

--- src/ydata/sdk/dataset/__init__.py
+++ src/ydata/sdk/dataset/__init__.py
@@ -1,3 +1,3 @@
-from .dataset import get_dataset
+from .dataset import get_dataset, get_census, get_airquality, get_titanic
 
-__all__ = ["get_dataset"]
+__all__ = ["get_dataset", "get_census", "get_airquality", "get_titanic"]
