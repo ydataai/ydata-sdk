248162e4e0b9ea03fb8b63fea2d3145eaca39c8d
ricardodcpereira <ricardo.pereira@ydata.ai>
2023-03-27 12:15:50 +0100

    fix: use privacy level enum from ydata-datascience

--- src/ydata/sdk/synthesizers/__init__.py
+++ src/ydata/sdk/synthesizers/__init__.py
@@ -1,4 +1,4 @@
-from ydata.core.synthesizers import PrivacyLevel
+from ydata.datascience.common import PrivacyLevel
 from ydata.sdk.synthesizers._models.synthesizers_list import SynthesizersList
 from ydata.sdk.synthesizers.regular import RegularSynthesizer
 from ydata.sdk.synthesizers.synthesizer import BaseSynthesizer as Synthesizer
--- src/ydata/sdk/synthesizers/synthesizer.py
+++ src/ydata/sdk/synthesizers/synthesizer.py
@@ -9,7 +9,7 @@ from pandas import DataFrame as pdDataFrame
 from pandas import read_csv
 from typeguard import typechecked
 
-from ydata.core.synthesizers import PrivacyLevel
+from ydata.datascience.common import PrivacyLevel
 from ydata.sdk.common.client import Client
 from ydata.sdk.common.client.utils import init_client
 from ydata.sdk.common.config import BACKOFF, LOG_LEVEL
