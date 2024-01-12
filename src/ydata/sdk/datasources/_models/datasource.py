daeab6071c31223fd3987a16c408b3dbfdfc3977
Luís Portela Afonso <luis.portela@ydata.ai>
2024-01-12 15:20:02 +0000

    remove commented code

--- src/ydata/sdk/datasources/_models/datasource.py
+++ src/ydata/sdk/datasources/_models/datasource.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Optional
 
 from ydata.sdk.common.types import UID
 from ydata.sdk.datasources._models.datatype import DataSourceType
@@ -10,27 +9,12 @@ from ydata.sdk.datasources._models.status import Status
 @dataclass
 class DataSource:
 
-    uid: Optional[UID] = None
-    author: Optional[str] = None
-    name: Optional[str] = None
-    datatype: Optional[DataSourceType] = None
-    metadata: Optional[Metadata] = None
-    status: Optional[Status] = None
-
-    def __post_init__(self):
-        if self.metadata is not None:
-            self.metadata = Metadata(**self.metadata)
-
-    #     if self.state is not None:
-    #         data = {
-    #             'validation': self.state.get('validation', {}).get('state', 'unknown'),
-    #             'metadata': self.state.get('metadata', {}).get('state', 'unknown'),
-    #             'profiling': self.state.get('profiling', {}).get('state', 'unknown')
-    #         }
-    #         self.state = State.parse_obj(data)
-
-    #     if self.status is not None:
-    #         self.status = Status(self.status)
+    uid: UID | None = None
+    author: str | None = None
+    name: str | None = None
+    datatype: DataSourceType | None = None
+    metadata: Metadata | None = None
+    status: Status | None = None
 
     def to_payload(self):
         return {}
