f913fb02ee406aadf7685d9ec4d13d76253bee3c
Fabiana Clemente <fabianaclemente@Fabianas-MacBook-Air.local>
2024-01-16 15:45:23 -0800

    docs: update multitable example.

--- examples/synthesizers/multi_table_quickstart.py
+++ examples/synthesizers/multi_table_quickstart.py
@@ -3,12 +3,12 @@ import os
 from ydata.sdk.datasources import DataSource
 from ydata.sdk.synthesizers import MultiTableSynthesizer
 
-# Authenticate to Fabric to leverage the SDK.
+# Authenticate to Fabric to leverage the SDK - https://docs.sdk.ydata.ai/latest/sdk/installation/
 # Make sure to add your token as env variable.
 os.environ["YDATA_TOKEN"] = '<TOKEN>'  # Remove if already defined
 
 # In this example, we demonstrate how to train a synthesizer from an existing RDBMS Dataset.
-# Make sure to follow the step-by-step guide to create a Dataset in Fabric's catalog: (add here the link)
+# Make sure to follow the step-by-step guide to create a Dataset in Fabric's catalog: https://docs.sdk.ydata.ai/latest/get-started/create_multitable_dataset/
 X = DataSource.get('<DATASOURCE_UID>')
 
 # Init a multi-table synthesizer. Provide a connector so that the process of data synthesis write the
@@ -22,4 +22,5 @@ synth.fit(X)
 # As soon as the training process is completed you are able to sample a synthetic database
 # The input expected is a percentage of the original database size
 # In this case it was requested a synthetic database with the same size as the original
+# Your synthetic sample was written to the database provided in the write_connector
 synth.sample(frac=1.)
