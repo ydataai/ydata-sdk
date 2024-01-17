7354ce2144a726fa128233f7300d5531ddf4a348
Luís Portela Afonso <luis.portela@ydata.ai>
2024-01-17 10:46:10 +0000

    feat: support to get resources from other projects (#84)

--- src/ydata/sdk/connectors/connector.py
+++ src/ydata/sdk/connectors/connector.py
@@ -8,7 +8,7 @@ from ydata.sdk.common.client.utils import init_client
 from ydata.sdk.common.config import LOG_LEVEL
 from ydata.sdk.common.exceptions import CredentialTypeError, InvalidConnectorError
 from ydata.sdk.common.logger import create_logger
-from ydata.sdk.common.types import UID
+from ydata.sdk.common.types import UID, Project
 from ydata.sdk.connectors._models.connector import Connector as mConnector
 from ydata.sdk.connectors._models.connector_list import ConnectorsList
 from ydata.sdk.connectors._models.connector_type import ConnectorType
@@ -25,6 +25,7 @@ class Connector(ModelFactoryMixin):
         connector_type (Union[ConnectorType, str]): Type of the connector to be created
         credentials (dict): Connector credentials
         name (Optional[str]): (optional) Connector name
+        project (Optional[Project]): (optional) Project name for this Connector
         client (Client): (optional) Client to connect to the backend
 
     Attributes:
@@ -32,10 +33,14 @@ class Connector(ModelFactoryMixin):
         type (ConnectorType): Type of the connector
     """
 
-    def __init__(self, connector_type: Union[ConnectorType, str] = None, credentials: Optional[Dict] = None,  name: Optional[str] = None, client: Optional[Client] = None):
+    def __init__(
+            self, connector_type: Union[ConnectorType, str] = None, credentials: Optional[Dict] = None,
+            name: Optional[str] = None, project: Optional[Project] = None, client: Optional[Client] = None):
         self._init_common(client=client)
         self._model: Optional[mConnector] = self._create_model(
             connector_type, credentials, name, client=client)
+
+        self._project = project
 
     @init_client
     def _init_common(self, client: Optional[Client] = None):
@@ -52,20 +57,22 @@ class Connector(ModelFactoryMixin):
 
     @staticmethod
     @init_client
-    def get(uid: UID, client: Optional[Client] = None) -> "Connector":
+    def get(uid: UID, project: Optional[Project] = None, client: Optional[Client] = None) -> "Connector":
         """Get an existing connector.
 
         Arguments:
             uid (UID): Connector identifier
-            client (Client): (optional) Client to connect to the backend
+            project (Optional[Project]): (optional) Project name from where to get the connector
+            client (Optional[Client]): (optional) Client to connect to the backend
 
         Returns:
             Connector
         """
-        connectors: ConnectorsList = Connector.list(client=client)
-        data = connectors.get_by_uid(uid)
-        model = mConnector(**data)
-        connector = ModelFactoryMixin._init_from_model_data(Connector, model)
+        response = client.get(f'/connector/{uid}', project=project)
+        data = response.json()
+        connector = Connector._init_from_model_data(Connector, mConnector(**data))
+        connector._project = project
+
         return connector
 
     @staticmethod
@@ -104,27 +111,34 @@ class Connector(ModelFactoryMixin):
         return _credentials
 
     @staticmethod
-    def create(connector_type: Union[ConnectorType, str], credentials: Union[str, Path, Dict, Credentials], name: Optional[str] = None, client: Optional[Client] = None) -> "Connector":
+    def create(
+        connector_type: Union[ConnectorType, str], credentials: Union[str, Path, Dict, Credentials],
+        name: Optional[str] = None, project: Optional[Project] = None, client: Optional[Client] = None
+    ) -> "Connector":
         """Create a new connector.
 
         Arguments:
             connector_type (Union[ConnectorType, str]): Type of the connector to be created
             credentials (dict): Connector credentials
             name (Optional[str]): (optional) Connector name
+            project (Optional[Project]): (optional) Project where to create the connector
             client (Client): (optional) Client to connect to the backend
 
         Returns:
             New connector
         """
         model = Connector._create_model(
-            connector_type=connector_type, credentials=credentials, name=name, client=client)
+            connector_type=connector_type, credentials=credentials, name=name, project=project, client=client)
         connector = ModelFactoryMixin._init_from_model_data(
             Connector, model)
+        connector._project = project
         return connector
 
     @classmethod
     @init_client
-    def _create_model(cls, connector_type: Union[ConnectorType, str], credentials: Union[str, Path, Dict, Credentials], name: Optional[str] = None, client: Optional[Client] = None) -> mConnector:
+    def _create_model(
+            cls, connector_type: Union[ConnectorType, str], credentials: Union[str, Path, Dict, Credentials],
+            name: Optional[str] = None, project: Optional[Project] = None, client: Optional[Client] = None) -> mConnector:
         _name = name if name is not None else str(uuid4())
         _connector_type = Connector._init_connector_type(connector_type)
         _credentials = Connector._init_credentials(_connector_type, credentials)
@@ -133,23 +147,24 @@ class Connector(ModelFactoryMixin):
             "credentials": _credentials.dict(by_alias=True),
             "name": _name
         }
-        response = client.post('/connector/', json=payload)
+        response = client.post('/connector/', project=project, json=payload)
         data: list = response.json()
 
         return mConnector(**data)
 
     @staticmethod
     @init_client
-    def list(client: Optional[Client] = None) -> ConnectorsList:
+    def list(project: Optional[Project] = None, client: Optional[Client] = None) -> ConnectorsList:
         """List the connectors instances.
 
         Arguments:
+            project (Optional[Project]): (optional) Project name from where to list the connectors
             client (Client): (optional) Client to connect to the backend
 
         Returns:
             List of connectors
         """
-        response = client.get('/connector')
+        response = client.get('/connector', project=project)
         data: list = response.json()
         return ConnectorsList(data)
 
