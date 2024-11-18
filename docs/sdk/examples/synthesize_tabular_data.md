4f1239db2c4fd34ef0b701d9bdfa9482b42ef5b0
Fabiana <30911746+fabclmnt@users.noreply.github.com>
2024-11-18 17:07:43 +0000

    docs: fix examples for time-series synthetic data generation & update docs

--- docs/sdk/examples/synthesize_tabular_data.md
+++ docs/sdk/examples/synthesize_tabular_data.md
@@ -2,6 +2,8 @@
 
 **Use YData's *RegularSynthesizer* to generate tabular synthetic data**
 
+For a more detailed tutorial please check [YData Fabric Academy ydata-sdk notebooks](https://github.com/ydataai/academy/tree/master).
+
 ```python
 --8<-- "examples/synthesizer_from_pandas.py"
 ```
--- examples/synthesizers/time_series_quickstart.py
+++ examples/synthesizers/time_series_quickstart.py
@@ -1,21 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Authentication
 import os
+# Do not forget to add your token as env variable
+os.environ["YDATA_TOKEN"] = '{insert-token}'
 
 from ydata.sdk.dataset import get_dataset
+
+# Sampling an example dataset for a multientity & multivariate time-series dataset"""
+
+# Generate the dataset
+time_series_data = get_dataset('timeseries')
+
+# Print the first few rows of the dataset
+print(time_series_data.head())
+
+## Train a Synthetic data generator
+
+### From a pandas dataframe
 from ydata.sdk.synthesizers import TimeSeriesSynthesizer
-
-# Do not forget to add your token as env variable
-os.environ["YDATA_TOKEN"] = '<TOKEN>'
-
-X = get_dataset('occupancy')
 
 # We initialize a time series synthesizer
 # As long as the synthesizer does not call `fit`, it exists only locally
-synth = TimeSeriesSynthesizer()
+synth = TimeSeriesSynthesizer(name='Time-series synth')
 
 # We train the synthesizer on our dataset
 # sortbykey -> variable that define the time order for the sequence
-synth.fit(X, sortbykey='date')
+synth.fit(time_series_data, sortbykey='time', entities='entity_id')
 
-# By default it is requested a synthetic sample with the same length as the original data
-# The TimeSeriesSynthesizer is designed to replicate temporal series and therefore the original time-horizon is respected
-sample = synth.sample(n_entities=1)
+### Generate samples from an already trained synthesizer
+### From the synthesizer in context in the notebook
+
+
+# Generate a sample with x number of entities
+# In this example the objective is to generate a dataset with the same size as the original. For that reason, 5 entities will be generated.
+sample = synth.sample(n_entities=5)
+
+sample.head()
+
+### From a previously trained synthetic data generation model
+# List the trained synthetic data generators to get the uid synthetisizer
+TimeSeriesSynthesizer.list()
+
+synth = TimeSeriesSynthesizer(uid='{insert-synth-id}').get()
+
+# Generate a new synthetic dataset with the sample method
+sample = synth.sample(n_entities=5)
+
+sample.head()
